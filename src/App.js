8159cce909605b27f6170ef2a4feb317ebfc2964
Leira C Sanchez Quinones <leira.sq@gmail.com>
2020-10-31 19:03:39 -0400

    added stopwatch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Board from 'react-trello';
+import data from "./data";
+import Timer from './Timer';
+import { useTimer } from './hooks'
 
-function App() {
+const App = () => {
+  const formatTime = (timer) => {
+    const getSeconds = `0${(timer % 60)}`.slice(-2)
+    const minutes = `${Math.floor(timer / 60)}`
+    const getMinutes = `0${minutes % 60}`.slice(-2)
+    const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`
+}
+
+  const handleDragStart = (cardId, laneId) => {
+    console.log({ cardId }, { laneId });
+  }
+
+  const Timer = () => {
+    const { timer, isActive, isPaused, handleStart, handlePause, handleResume, handleReset } = useTimer(0)
+    return (
+      <div className="app">
+            <h3>React Stopwatch</h3>
+            <div className='stopwatch-card'>
+                <p>{formatTime(timer)}</p>
+                <div className='buttons'>
+                    {
+                        !isActive && !isPaused ?
+                            <button onClick={handleStart}>Start</button>
+                            : (
+                                isPaused ? <button onClick={handlePause}>Pause</button> :
+                                    <button onClick={handleResume}>Resume</button>
+                            )
+                    }
+                    <button onClick={handleReset} disabled={!isActive}>Reset</button>
+                </div>
+            </div>
+        </div>
+    )
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>PETVET</h1>
+      <Timer />
+      <Board
+        data={data}
+        handleDragStart={handleDragStart}
+      />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/data.js
@@ -0,0 +1,66 @@
+const data = {
+    lanes: [
+      {
+        id: "lane1",
+        title: "Checked-In",
+        label: "this is a label",
+        cards: [
+          {
+            id: "Card1",
+            title: "Blue",
+            description: "Gato",
+            label: "Nail trim"
+          },
+          {
+            id: "Card2",
+            title: "Lola",
+            description: "Dog - Maltese",
+            label: "5 mins",
+            metadata: { sha: "be312a1" }
+          }
+        ]
+      },
+      {
+        id: "lane2",
+        title: "Vet Tech",
+        label: "0/0",
+        cards: [
+          {
+            id: "Card1-2",
+            title: "Sony",
+            description: "Dog - Chihuahua",
+            label: "Vaccines"
+          }
+        ]
+      },
+      {
+        id: "lane3",
+        title: "Vet",
+        label: "0/0",
+        cards: [
+          {
+            id: "Card1-2",
+            title: "Nico",
+            description: "Dog - Chihuahua",
+            label: "Vomiting"
+          }
+        ]
+      },
+      {
+        id: "lane4",
+        title: "Chcked-Out",
+        label: "0/0",
+        cards: [
+          {
+            id: "Card1-2",
+            title: "Bleachy",
+            description: "Python",
+            label: "Routine",
+            draggable: false
+          }
+        ]
+      }
+    ]
+  };
+  export default data;
+  
